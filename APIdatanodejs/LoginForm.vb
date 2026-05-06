13f1dfe6f12f81a2a7274bbf7b87c62e86bb7b96
aziz2790 <muslim.aziz27@gmail.com>
2026-05-06 15:07:31 +0700

    up060526

--- APIdatanodejs/LoginForm.vb
+++ APIdatanodejs/LoginForm.vb
@@ -11,7 +11,7 @@ Public Class LoginForm
                 Return
             End If
 
-            Dim loginUrl As String = "http://localhost:5000/api/auth/login"
+            Dim loginUrl As String = "http://192.168.9.7:3008/api/auth/login"
             Dim loginJson As String = "{""username"":""" & UsernameTextBox.Text & """, ""password"":""" & PasswordTextBox.Text & """}"
 
             Dim loginRequest As HttpWebRequest = DirectCast(WebRequest.Create(loginUrl), HttpWebRequest)
@@ -29,8 +29,19 @@ Public Class LoginForm
 
                 Dim serializer As New JavaScriptSerializer()
                 Dim responseData As Dictionary(Of String, Object) = serializer.Deserialize(Of Dictionary(Of String, Object))(result)
+                'MessageBox.Show(result)
+                If responseData.ContainsKey("status") AndAlso
+                    responseData("status").ToString().ToLower() = "true" Then
 
-                token = responseData("token").ToString()
+                    Dim dataDict As Dictionary(Of String, Object) =
+                        CType(responseData("data"), Dictionary(Of String, Object))
+
+                    token = dataDict("token").ToString()
+
+                Else
+                    MessageBox.Show("Login gagal.")
+                    Return
+                End If
 
                 ' Beri tahu Form1 tokennya
                 txtItemNamaCustomer.SetToken(token, UsernameTextBox.Text)
